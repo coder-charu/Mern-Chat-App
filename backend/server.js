67d5b7e46a319ad5485f4ac492827e687b576aac
coder-charu <charu9670@gmail.com>
2026-07-01 13:17:37 +0530

    db connected & add user model

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoute from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT;
@@ -12,4 +13,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/auth", authRoute);
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+app.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`Server is running on port ${PORT}`);
+});
